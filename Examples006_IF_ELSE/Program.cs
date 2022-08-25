9ce3c216b793f85a433d0fbc9583a9c78f375f76
Eugenius <eugenius8447@gmail.com>
2022-08-25 17:57:58 +0300

    Операторы if,else

---
+++ Examples006_IF_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Кто ж тебя сюда звал,МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
